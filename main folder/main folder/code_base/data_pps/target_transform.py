63c8494b632744d7f9809e398662793d05c5c036
Anirban0011 <bhattandbhatt.999@gmail.com>
2025-02-21 14:06:36 +0530

    update files

--- main folder/main folder/code_base/data_pps/target_transform.py
+++ main folder/main folder/code_base/data_pps/target_transform.py
@@ -1,11 +1,12 @@
 from lifelines import KaplanMeierFitter
 import pandas as pd
+import numpy as np
 
 class TargetTransform():
     def __init__(self):
         pass
     '''
-    train["y"] = return 
+    train["y"] = return
     '''
     def basic_transform(self, train : pd.DataFrame):
         train["y"] = train.efs_time.values
@@ -15,6 +16,19 @@ class TargetTransform():
         train.y = train.y.rank()
         train.loc[train.efs==0,"y"] += len(train)//2
         train.y = train.y / train.y.max()
+        return train.y
+
+    def basic_transform2(self, train : pd.DataFrame):
+        train["y"] = train.efs_time.values
+        mx = train.loc[train.efs==1,"efs_time"].max()
+        mn = train.loc[train.efs==0,"efs_time"].min()
+        train.loc[train.efs==0,"y"] = train.loc[train.efs==0,"y"] + mx - mn
+        train.y = train.y.rank()
+        train.loc[train.efs==0,"y"] += 2*len(train)
+        train.y = train.y / train.y.max()
+        train.y = np.log( train.y )
+        train.y -= train.y.mean()
+        train.y *= -1.0
         return train.y
 
     def KMF_transform(self, train : pd.DataFrame, time_col='efs_time', event_col='efs'):
